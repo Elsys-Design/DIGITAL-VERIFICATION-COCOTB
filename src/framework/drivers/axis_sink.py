04db325421cb8b6cd3213ff88bf2ae4674631d77
Paul Suau <paul.suau@elsys-design.com>
2023-11-10 10:48:20 +0100

    AxiStreamSink saves the received data
    So it can be written back to a file

--- src/framework/drivers/axis_sink.py
+++ src/framework/drivers/axis_sink.py
@@ -12,11 +12,13 @@ class AxiStreamSink(cocotbext.axi.axis.AxiStreamBase):
     def __init__(self, bus, clock, reset=None, reset_active_level=True, **kwargs): 
         super().__init__(bus, clock, reset, reset_active_level, **kwargs)
 
-        self.current_accept = 0
+        self.current_idx = 0
+        self.current_data = None
         self.read_done = Event()
 
     async def read_data(self, data):
-        self.current_accept = len(data.data)
+        self.current_idx = 0
+        self.current_data = data
         await self.read_done.wait()
         self.read_done.clear()
 
@@ -42,7 +44,7 @@ class AxiStreamSink(cocotbext.axi.axis.AxiStreamBase):
             self.bus.tready.value = 1
             return
 
-        bus_size = len(self.bus.tdata) // 8
+        bus_size = len(self.bus.tdata)//8
         has_tkeep = hasattr(self.bus, "tkeep")
 
         self.bus.tready.value = 0
@@ -50,15 +52,24 @@ class AxiStreamSink(cocotbext.axi.axis.AxiStreamBase):
         while True:
             await RisingEdge(self.clock)
 
-            if self.current_accept > 0:
+            if self.current_data:
                 self.bus.tready.value = 1
 
             if self.bus.tready.value and self.bus.tvalid.value:
-                self.current_accept -= bus_size if not has_tkeep else self.bus.tkeep.value.count(1)
+                if has_tkeep:
+                    for i in range(len(self.bus.tkeep.value)):
+                        if self.bus.tkeep.value[i]:
+                            self.current_data.data[self.current_idx] = self.bus.tdata.value.buff[i]
+                            self.current_idx += 1
+                else:
+                    self.current_data.data[self.current_idx:self.current_idx+bus_size] = self.bus.tdata.value.buff
+                    self.current_idx += bus_size
                 
                 # if the bus has no tkeep, we can receive more bytes than we want and there's nothing we can do about it
-                if self.current_accept <= 0:
+                if self.current_idx >= len(self.current_data.data):
                     self.bus.tready.value = 0
+                    self.current_idx = 0
+                    self.current_data = None
                     self.read_done.set()
 
 
