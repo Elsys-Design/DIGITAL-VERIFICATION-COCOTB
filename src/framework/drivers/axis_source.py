ba17bbbb813ed05fd75f83a166fc008ced33758a
Paul Suau <paul.suau@elsys-design.com>
2023-11-10 15:51:29 +0100

    Fixed interconnect_stream golden_* + fixed AxiStreamSource
    AxiStreamSource supposed there was a tlast and a tkeep signal, it's not
    verified before handling the specific behaviour associated.

--- src/framework/drivers/axis_source.py
+++ src/framework/drivers/axis_source.py
@@ -17,13 +17,16 @@ class AxiStreamSource(cocotbext.axi.AxiStreamSource):
 
         self.bus_size = len(self.bus.tdata.value) // 8
 
+        self.has_tkeep = hasattr(self.bus, "tkeep")
+        self.has_tlast = hasattr(self.bus, "tlast")
+
     async def write_data(self, data):
         # Just a warning in case there is no tkeep but the data doesn't fit exactly in the bus
-        if not hasattr(self.bus, "tkeep") and len(data.data) % self.bus_size  != 0:
-            logger.warning("Writing data of size {} to AxiStream bus of size {} with no tkeep -> {} 0x0 bytes will" \
-                    "be added to the transaction".format(len(data.data), self.bus_size, n%self.bus_size))
+        if not self.has_tkeep and len(data.data) % self.bus_size  != 0:
+            logger.warning("Writing data of size {} to AxiStream bus of size {} with no tkeep -> {} * 0x0 bytes will" \
+                    "be added to the transaction".format(len(data.data), self.bus_size, len(data.data)%self.bus_size))
 
-        if not data.stream_tlast_end:
+        if self.has_tlast and not data.stream_tlast_end:
             cocotb.start_soon(self._remove_one_tlast())
 
         # tkeep is supported by default by cocotbext.axi.AxiStreamSource
