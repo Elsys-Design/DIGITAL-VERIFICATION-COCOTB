835d4c06f9e7eccc57dc6591664413897092a9ee
Paul Suau <paul.suau@elsys-design.com>
2024-01-15 09:30:27 +0100

    Fixed warning using "name" instead of "_name"

--- src/framework/utils.py
+++ src/framework/utils.py
@@ -2,7 +2,7 @@ def int_to_hex_string(word, word_size):
     return "0x{word:0{word_size}X}".format(word=word, word_size=word_size * 2)
 
 def get_full_bus_name(bus):
-    name = bus._entity.name
+    name = bus._entity._name
 
     # adding bus name
     if bus._name is not None:
